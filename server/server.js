ee42222f875570917e612bad68ee8fa3b928af36
Nesib Keleshov <nesib@Aztrade_LLC.local>
2025-04-09 15:37:31 +0400

    PostgreSQL DB ve DB Session inteqrasiyasi

--- server/server.js
+++ server/server.js
@@ -924,7 +924,7 @@ io.on('connection', (socket) => {
     // handleDisconnectOrLeave funksiyasının sonu (Part 4-dən davam)
   } // handleDisconnectOrLeave sonu
 
-}); // <--- io.on('connection', ...) blokunun bağlanması BURADADIR!
+); // <--- io.on('connection', ...) blokunun bağlanması BURADADIR!
 
 console.log('[Setup] Socket.IO \'connection\' dinləyicisi təyin edildi.');
 
